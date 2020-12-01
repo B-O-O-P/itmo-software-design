493ea2bcb168bee357a4a096e72d4d27254bdc5a
Chizhikov Dmitriy <dimkanut@gmail.com>
2020-12-01 16:27:27 +0300

    fancy main

--- hw6/src/main/java/ru/itmo/chizhikov/Main.java
+++ hw6/src/main/java/ru/itmo/chizhikov/Main.java
@@ -1,7 +1,33 @@
 package ru.itmo.chizhikov;
 
-public class Main {
-    public static void main(String[] args) {
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.List;
 
+import ru.itmo.chizhikov.token.Token;
+import ru.itmo.chizhikov.token.Tokenizer;
+import ru.itmo.chizhikov.visitor.CalcVisitor;
+import ru.itmo.chizhikov.visitor.ParserVisitor;
+import ru.itmo.chizhikov.visitor.PrintVisitor;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParseException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Tokenizer tokenizer = new Tokenizer();
+        PrintVisitor printVisitor = new PrintVisitor();
+        CalcVisitor calcVisitor = new CalcVisitor();
+
+        String rpn = reader.readLine();
+        ParserVisitor parserVisitor = new ParserVisitor();
+
+        List<Token> tokens = tokenizer.parse(rpn);
+        System.out.println(printVisitor.print(tokens));
+
+        List<Token> rpnTokens = parserVisitor.parse(tokens);
+        System.out.println(printVisitor.print(rpnTokens));
+
+        System.out.println(calcVisitor.calculate(rpnTokens));
+    }
 }
