ae94a2b371bbca22f41f1bbc0179ae7ed1490ad7
Chizhikov Dmitriy <dimkanut@gmail.com>
2020-12-01 16:20:24 +0300

    visitors added

---
+++ hw6/src/main/java/ru/itmo/chizhikov/visitor/ParserVisitor.java
@@ -0,0 +1,61 @@
+package ru.itmo.chizhikov.visitor;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import ru.itmo.chizhikov.token.Token;
+import ru.itmo.chizhikov.token.TokenType;
+import ru.itmo.chizhikov.token.tokens.BraceToken;
+import ru.itmo.chizhikov.token.tokens.NumberToken;
+import ru.itmo.chizhikov.token.tokens.OperationToken;
+
+public class ParserVisitor implements TokenVisitor {
+    private final List<Token> rpnTokens = new ArrayList<>();
+    private final Deque<Token> stack = new ArrayDeque<>();
+
+    public List<Token> parse(List<Token> tokens) {
+        tokens.forEach(token -> token.accept(this));
+
+        while (!stack.isEmpty()) {
+            rpnTokens.add(stack.pollLast());
+        }
+
+        return new ArrayList<>(rpnTokens);
+    }
+
+    @Override
+    public void visit(NumberToken token) {
+        rpnTokens.add(token);
+    }
+
+    @Override
+    public void visit(BraceToken token) {
+        if (token.getTokenType().equals(TokenType.LEFT_BRACE)) {
+            stack.add(token);
+        } else {
+            if (stack.isEmpty()) {
+                throw new IllegalStateException();
+            }
+
+            Token nextToken = stack.pollLast();
+            while (!stack.isEmpty() && !nextToken.getTokenType().equals(TokenType.LEFT_BRACE)) {
+                rpnTokens.add(nextToken);
+                nextToken = stack.pollLast();
+            }
+        }
+    }
+
+    @Override
+    public void visit(OperationToken token) {
+        if (!stack.isEmpty()) {
+            Token nextToken = stack.peekLast();
+            while (!stack.isEmpty() && token.getTokenType().getPriority() <= nextToken.getTokenType().getPriority()) {
+                rpnTokens.add(stack.pollLast());
+                nextToken = stack.peekLast();
+            }
+        }
+        stack.add(token);
+    }
+
+}
---
+++ hw6/src/main/java/ru/itmo/chizhikov/visitor/PrintVisitor.java
@@ -0,0 +1,38 @@
+package ru.itmo.chizhikov.visitor;
+
+import java.util.List;
+import ru.itmo.chizhikov.token.Token;
+import ru.itmo.chizhikov.token.tokens.BraceToken;
+import ru.itmo.chizhikov.token.tokens.NumberToken;
+import ru.itmo.chizhikov.token.tokens.OperationToken;
+
+public class PrintVisitor implements TokenVisitor {
+    StringBuilder builder = new StringBuilder();
+
+    public String print(List<Token> tokens) {
+        tokens.forEach(token -> token.accept(this));
+        String result = builder.toString();
+        builder = new StringBuilder();
+        return result;
+    }
+
+    @Override
+    public void visit(NumberToken token) {
+        print(token);
+    }
+
+    @Override
+    public void visit(BraceToken token) {
+        print(token);
+    }
+
+    @Override
+    public void visit(OperationToken token) {
+        print(token);
+    }
+
+    private void print(Token token) {
+        builder.append(token).append(" ");
+    }
+
+}
