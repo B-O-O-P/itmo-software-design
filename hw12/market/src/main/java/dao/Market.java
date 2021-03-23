635eb96b81c250be848da52bd0ff7edc9b0fec62
Chizhikov Dmitriy <dimkanut@gmail.com>
2021-03-23 20:19:37 +0300

    [HW 12] init market dao

---
+++ hw12/market/src/main/java/dao/Market.java
@@ -0,0 +1,20 @@
+
+package dao;
+
+import com.mongodb.rx.client.Success;
+import model.Stocks;
+import rx.Observable;
+
+public interface Market {
+    Observable<Success> addCompany(String name, int stocksCount, int stocksPrice);
+
+    Observable<Stocks> getCompanies();
+
+    Observable<Success> addStocks(String companyName, int stocksCount);
+
+    Observable<Stocks> getStocksInfo(String companyName);
+
+    Observable<Success> buyStocks(String companyName, int count);
+
+    Observable<Success> changeStocksPrice(String companyName, int newStocksPrice);
+}
